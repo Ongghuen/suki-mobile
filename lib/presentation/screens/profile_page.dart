e69b66af3e522fb4072d1c26421310ab0467d2e2
Raihan <17radf@gmail.com>
2022-11-19 09:48:51 +0700

    fix: merge branch not merged💀

--- lib/presentation/screens/profile_page.dart
+++ lib/presentation/screens/profile_page.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-
-class UserProfile extends StatelessWidget {
-  const UserProfile({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-
-=======
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
->>>>>>> add-features.raihan
     TextEditingController names = TextEditingController();
     TextEditingController username = TextEditingController();
     TextEditingController email = TextEditingController();
@@ -25,35 +15,16 @@ class ProfilePage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.only(top: 50.0),
           child: Stack(children: <Widget>[
-<<<<<<< HEAD
-              Container(
-                margin: EdgeInsets.only(top: 80.0),
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-=======
             Container(
               margin: EdgeInsets.only(top: 80.0),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
               decoration: BoxDecoration(
->>>>>>> add-features.raihan
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(40),
                     topRight: Radius.circular(40),
                   )),
-<<<<<<< HEAD
-                child: Column(
-                  children: <Widget>[
-                  SizedBox(
-                    height: 80.0,
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 40.0,left: 20.0, right: 20.0),
-                      child: Column(
-                        children: <Widget>[
-=======
               child: Column(
                 children: <Widget>[
                   SizedBox(
@@ -64,7 +35,6 @@ class ProfilePage extends StatelessWidget {
                         EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
                     child: Column(
                       children: <Widget>[
->>>>>>> add-features.raihan
                         TextField(
                           controller: names,
                           decoration: InputDecoration(
@@ -75,13 +45,9 @@ class ProfilePage extends StatelessWidget {
                             focusedBorder: myFocusBorder(),
                           ),
                         ),
-<<<<<<< HEAD
-                        SizedBox(height: 20.0,),
-=======
                         SizedBox(
                           height: 20.0,
                         ),
->>>>>>> add-features.raihan
                         TextField(
                           controller: username,
                           decoration: InputDecoration(
@@ -92,13 +58,9 @@ class ProfilePage extends StatelessWidget {
                             focusedBorder: myFocusBorder(),
                           ),
                         ),
-<<<<<<< HEAD
-                        SizedBox(height: 20.0,),
-=======
                         SizedBox(
                           height: 20.0,
                         ),
->>>>>>> add-features.raihan
                         TextField(
                           controller: email,
                           decoration: InputDecoration(
@@ -109,13 +71,9 @@ class ProfilePage extends StatelessWidget {
                             focusedBorder: myFocusBorder(),
                           ),
                         ),
-<<<<<<< HEAD
-                        SizedBox(height: 20.0,),
-=======
                         SizedBox(
                           height: 20.0,
                         ),
->>>>>>> add-features.raihan
                         TextField(
                           controller: password,
                           decoration: InputDecoration(
@@ -130,50 +88,11 @@ class ProfilePage extends StatelessWidget {
                           height: 20.0,
                         ),
                         ElevatedButton(
-<<<<<<< HEAD
-                          onPressed: () {}, 
-=======
                           onPressed: () {},
->>>>>>> add-features.raihan
                           child: Text('Update'),
                           style: ElevatedButton.styleFrom(
                               primary: Colors.black,
                               padding: EdgeInsets.symmetric(
-<<<<<<< HEAD
-                                  horizontal: 25.0,vertical: 20.0),
-                              textStyle: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold)),
-                          ),
-                      ],
-                      ),
-                    )
-                ],),
-              ), 
-              Align(
-              alignment: Alignment.topCenter,
-              child: Stack(
-                children: <Widget>[
-                  ClipOval(
-                    child: Image.asset(
-                      'images/polije.jpg',
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.cover,
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 5,
-                    right: 15.5,
-                    child: Container(
-                      padding: EdgeInsets.all(5.0),
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,shape: BoxShape.circle
-                      ),
-                      child: Icon(Icons.edit,size: 30.0,)),
-                  )
-                  ],
-                ),
-=======
                                   horizontal: 25.0, vertical: 20.0),
                               textStyle: TextStyle(
                                   fontSize: 20, fontWeight: FontWeight.bold)),
@@ -183,33 +102,11 @@ class ProfilePage extends StatelessWidget {
                   )
                 ],
               ),
->>>>>>> add-features.raihan
             ),
           ]),
         ),
       ),
     );
-<<<<<<< HEAD
-       
-  }
-  OutlineInputBorder myInputBorder(){
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      borderSide: BorderSide(
-        color: Colors.black,
-        width: 3,
-        ) );
-  }
-  OutlineInputBorder myFocusBorder(){
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      borderSide: BorderSide(
-        color: Color(0xffcbf49),
-        width: 3,
-        ) );
-  }
-}
-=======
   }
 
   OutlineInputBorder myInputBorder() {
@@ -230,4 +127,3 @@ class ProfilePage extends StatelessWidget {
         ));
   }
 }
->>>>>>> add-features.raihan
