c8a5cc38e803fcf3a4e94eaff35d5e3d67e17820
Raihan <17radf@gmail.com>
2022-12-31 20:56:54 +0700

    feat: use heroku url and network permission

--- lib/presentation/utils/default.dart
+++ lib/presentation/utils/default.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 
-const url = "http://192.168.0.103:8000";
+// const url = "http://192.168.0.103:8000";
 // const url = "http://10.0.2.2:8000";
-// const url = "https://sukifurniture.herokuapp.com";
+const url = "https://sukifurniture.herokuapp.com";
 
 // url api yang dipake
 const apiUrl = url;
